fa2cb1c1d8b7a167f0d7023fd9bd68b7099a8c2e
jisdtn <122818010+jisdtn@users.noreply.github.com>
2025-02-07 19:11:37 +0800

    add two_sum solution

---
+++ two_sum.py
@@ -0,0 +1,23 @@
+def find_two_indexes(data, expected_result):
+    left_pointer = 0
+    right_pointer = len(data) - 1
+
+    while left_pointer < right_pointer:
+        current_sum = data[left_pointer] + data[right_pointer]
+
+        if current_sum == expected_result:
+            return True
+
+        if current_sum > expected_result:
+            right_pointer -= 1
+
+        else:
+            left_pointer += 1
+
+    return False
+
+
+if __name__ == '__main__':
+    data = [1, 2, 3, 4, 5, 6, 7, 11]
+    expected_result = 10
+    print(find_two_indexes(data, expected_result))
