b6b6f88699935a2385c1481e1436b2fd442be4a5
jisdtn <122818010+jisdtn@users.noreply.github.com>
2025-04-18 20:42:15 +0200

    add reorder list solution

--- solutions/reorder_list.py
+++ solutions/reorder_list.py
@@ -5,11 +5,13 @@
 #
 # L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
 # You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-from typing import Optional
 
 
 class Solution:
@@ -40,3 +42,29 @@ class Solution:
             second.next = tmp1
             first = tmp1
             second = tmp2
+
+def list_to_linked_list(values: list) -> Optional[ListNode]:
+    if not values:
+        return None
+    head = ListNode(values[0])
+    curr = head
+    for val in values[1:]:
+        curr.next = ListNode(val)
+        curr = curr.next
+    return head
+
+
+def linked_list_to_list(head: ListNode) -> list:
+    result = []
+
+    while head:
+        result.append(head.val)
+        head = head.next
+    return result
+
+
+if __name__ == '__main__':
+    input_list = [1,2,3,4]
+    head = list_to_linked_list(input_list)
+    Solution().reorderList(head)
+    print(linked_list_to_list(head))
